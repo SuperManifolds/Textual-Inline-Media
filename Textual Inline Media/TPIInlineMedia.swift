e40936801453ad2f2e7a681bf0405dfb7ed2bcea
Alex S. Glomsaas <alex@sorlie.co.uk>
2015-08-31 13:50:54 +0200

    Initial Commit

---
+++ Textual Inline Media/TPIInlineMedia.swift
@@ -0,0 +1,14 @@
+//
+//  TPIInlineMedia.swift
+//  Textual Inline Media
+//
+//  Copyright © 2015 Alex S. Glomsaas. All rights reserved.
+//
+
+import Foundation
+
+class TPIInlineMedia: NSObject, THOPluginProtocol {
+    func pluginLoadedIntoMemory() {
+        
+    }
+}
